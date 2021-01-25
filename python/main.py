c92f826b3c2ef4699891c673e3c8426c279f0719
Siocnarff <josuabotha@protonmail.com>
2021-01-25 06:52:06 +0200

    add gitignore, create media folder for all vidoes and photos, add config file

--- python/main.py
+++ python/main.py
@@ -4,11 +4,11 @@ import os
 folder = input("Folder In data To Read From: ")
 
 try: 
-    if not os.path.exists(f'out/{folder}'): 
-        os.makedirs(f'out/{folder}') 
+    if not os.path.exists(f'media/out/{folder}'): 
+        os.makedirs(f'media/out/{folder}') 
 except OSError: 
     print ('Error: Creating directory for out')
 
 fileName = 0
-while rr.analyzeFrame("data/" + folder + "/", str(fileName) + ".jpg", "out/" + folder + "/"):
+while rr.analyzeFrame("media/data/" + folder + "/", str(fileName) + ".jpg", "media/out/" + folder + "/"):
     fileName += 1
